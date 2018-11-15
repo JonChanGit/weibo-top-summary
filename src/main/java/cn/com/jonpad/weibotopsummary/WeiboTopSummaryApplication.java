87c1f3406463b24abe48c3494f242436fa245e95
JonChan <jonchan.hv@outlook.com>
2018-11-15 22:05:34 +0800

    初始化项目

---
+++ src/main/java/cn/com/jonpad/weibotopsummary/WeiboTopSummaryApplication.java
@@ -0,0 +1,12 @@
+package cn.com.jonpad.weibotopsummary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeiboTopSummaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeiboTopSummaryApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/com/jonpad/weibotopsummary/WeiboTopSummaryApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.com.jonpad.weibotopsummary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeiboTopSummaryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
