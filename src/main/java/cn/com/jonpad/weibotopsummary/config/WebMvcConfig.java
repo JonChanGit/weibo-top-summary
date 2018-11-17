5d9b0119e4de678fa91c493827d1355061980bf0
JonChan <jonchan.hv@outlook.com>
2018-11-17 15:28:43 +0800

    版本0.1.3-Hydrogen 允许跨域访问

--- src/main/java/cn/com/jonpad/weibotopsummary/config/WebMvcConfig.java
+++ src/main/java/cn/com/jonpad/weibotopsummary/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package cn.com.jonpad.weibotopsummary.config;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -18,5 +19,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
         converters.add(0, converter);
+    }
+
+    /**
+     * 允许跨域访问
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/top-summary").allowedOrigins("*").allowedMethods("GET");
     }
 }
